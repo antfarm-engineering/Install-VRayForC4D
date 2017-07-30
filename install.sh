6cb48f83099a20ae5264ce0e6886f2f7e851d004
sfrankiel <sfrankiel@Gmail.com>
2017-07-30 15:48:49 -0700

    Switching from copying files to extracting tar.gz

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #
 # Created by Sam Frankiel
 # Creation date 7/28/2017
-# Modified date 
+# Modified date 7/30/2017
 #
 
 #Verify script is being run as root
@@ -14,6 +14,8 @@ fi
 
 #Find the directory that the script is being run from 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#Set path variables
 R17PATH="/Applications/MAXON/CINEMA\ 4D\ R17"
 R18PATH="/Applications/MAXON/CINEMA\ 4D\ R18"
 CGPATH="/Applications/ChaosGroup"
@@ -29,12 +31,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	if	[[ $REPLY == 1 ]]; then
 		#Check if R17 is installed - if not move to R18
 		if [ -d "$R17PATH" ]; then  
-			cp -R $DIR/C4DR17-C4DR17App-Contents-MacOS /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/
-			cp -R $DIR/Library-MatPreviews/* /Applications/MAXON/CINEMA4D\ R17/library/materialpreviews
-			printf "Changing R17 Permissions\n"
-			chmod -R 755 /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/lib*
-			chmod -R 755 /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/vray_stuff.dylib
-			chmod -R 755 /Applications/MAXON/CINEMA4D\ R17/library/materialpreviews
+			tar xf $DIR/AppContentsR17.tar.gz /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/
+			tar xf $DIR/materialpreviews.tar.gz /Applications/MAXON/CINEMA4D\ R17/library/materialpreviews
 		else
 			printf "Cinema 4D R17 folder not detected\n"
 			:
@@ -42,11 +40,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	elif [[ $REPLY == 2 ]]; then
 		#Check if R18 is installed - if not move to exit
 		if [ -d "$R18PATH" ]; then  
-			cp -R $DIR/C4DR18-C4DR18App-Contents-MacOS /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/
-			cp -R $DIR/Library-MatPreviews/* /Applications/MAXON/CINEMA4D\ R18/library/materialpreviews
-			chmod -R 755 /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/lib*
-			chmod -R 755 /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/vray_stuff.dylib
-			chmod -R 755 /Applications/MAXON/CINEMA4D\ R18/library/materialpreviews
+			tar xf $DIR/AppContentsR17.tar.gz /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/
+			tar xf $DIR/materialpreviews.tar.gz /Applications/MAXON/CINEMA4D\ R18/library/materialpreviews
 		else
 			printf "Cinema 4D R18 folder not detected\n"
 			:
@@ -57,24 +52,17 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 			exit
 		else
 			#Check if R17 is installed - if not move to R18
-			if [ -d "$R17PATH" ]; then  
-				cp -R $DIR/C4DR17-C4DR17App-Contents-MacOS /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/
-				cp -R $DIR/Library-MatPreviews/* /Applications/MAXON/CINEMA4D\ R17/library/materialpreviews
-				printf "Changing R17 Permissions\n"
-				chmod -R 755 /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/lib*
-				chmod -R 755 /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/vray_stuff.dylib
-				chmod -R 755 /Applications/MAXON/CINEMA4D\ R17/library/materialpreviews
+			if [ -d "$R17PATH" ]; then
+				tar xf $DIR/AppContentsR17.tar.gz /Applications/MAXON/CINEMA4D\ R17/CINEMA4D\ R17.app/Contents/MacOS/
+				tar xf $DIR/materialpreviews.tar.gz /Applications/MAXON/CINEMA4D\ R17/library/materialpreviews
 			else
 				printf "Cinema 4D R17 folder not detected\n"
 				:
 			fi
 			#Check if R18 is installed - if not move to exit
-			if [ -d "$R18PATH" ]; then  
-				cp -R $DIR/C4DR18-C4DR18App-Contents-MacOS /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/
-				cp -R $DIR/Library-MatPreviews/* /Applications/MAXON/CINEMA4D\ R18/library/materialpreviews
-				chmod -R 755 /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/lib*
-				chmod -R 755 /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/vray_stuff.dylib
-				chmod -R 755 /Applications/MAXON/CINEMA4D\ R18/library/materialpreviews
+			if [ -d "$R18PATH" ]; then
+				tar xf $DIR/AppContentsR17.tar.gz /Applications/MAXON/CINEMA4D\ R18/CINEMA4D\ R18.app/Contents/MacOS/
+				tar xf $DIR/materialpreviews.tar.gz /Applications/MAXON/CINEMA4D\ R18/library/materialpreviews
 			else
 				printf "Cinema 4D R18 folder not detected\n"
 				:
@@ -83,14 +71,16 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	fi
 	#Check if Chaos Group stuff is installed - if not create Chaos Group directory and copy files
 	if [ -d "$CGPATH" ]; then 
-		cp -R $DIR/Applications-ChaosGroup /Applications/ChaosGroup/
-		chmod -R 755 /Applications/ChaosGroup/bin
+		tar xf $DIR/CGBinaries.tar.gz /Applications/ChaosGroup
+		# chmod -R 755 /Applications/ChaosGroup/bin
 	else
 		printf "Maya/Max Chaos Group plugins not detected - creating Chaos Group folder\n"
 		mkdir /Applications/ChaosGroup
 		chmod 755 /Applications/ChaosGroup
-		cp -R $DIR/Applications-ChaosGroup /Applications/ChaosGroup/
-		chmod -R 755 /Applications/ChaosGroup/bin
+		tar xf $DIR/CGBinaries.tar.gz /Applications/ChaosGroup
 	fi
+	# read -p "It looks like you are installing on "$HOSTNAME". Do you want to automatically install the license for this host? (y/n) - Default y" -n 1 -r
+	# if [[ $REPLY =~ ^[Yy]$ ]]; then
+	# 	if [[$HOSTNAME == USA]]
 fi
 exit
